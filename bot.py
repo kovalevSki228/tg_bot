8a905a6c7a057c836870f74f948d5e0869212175
ilya.kovalevski <ilya.kovalevski@abfprogramming.com>
2025-03-17 10:55:08 +0100

    some fixed

--- bot.py
+++ bot.py
@@ -1,44 +1,46 @@
 import os
 import telebot
 from telebot.types import BotCommand
-print("Environment Variables:", os.environ)
+
+# Получаем токен
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 if not TOKEN:
     raise ValueError("Ошибка: TELEGRAM_BOT_TOKEN не задан!")
 
 bot = telebot.TeleBot(TOKEN)
-# Список команд для бота
+
 commands = [
     BotCommand("all", "Тегнуть всех"),
     BotCommand("repo", "Тегнуть в репу"),
 ]
-
-# Устанавливаем команды
 bot.set_my_commands(commands)
 
-# Define a handler for text messages
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    message_text = message.text.split()
-    try:
-        if (message_text[0] == '@all'):
-            bot.send_message(
-                message.chat.id,
-                f"@i_bojenka @crownvagen @kosoy06 @fursten1 @AquaDarida @karanik_y @youlvly @danilamankevich @luvrzinc {message_text}"
-            )
-        if (message_text[0] == '@repo'):
-            bot.send_message(
-                message.chat.id,
-                f"@i_bojenka @crownvagen @kosoy06 @fursten1 @AquaDarida @karanik_y в репу"
-            )
-    except ValueError:
-        bot.send_message(message.chat.id, "Иди нахуй")
+REPO_PLAYERS = [
+    "@i_bojenka",
+    "@crownvagen",
+    "@kosoy06",
+    "@fursten1",
+    "@AquaDarida",
+]
 
-# Define a handler for text, document, and audio messages
+def get_admins_list(chat_id):
+    admins = bot.get_chat_administrators(chat_id)
+    return [f"@{admin.user.username}" for admin in admins]
+
+@bot.message_handler(commands=["repo"])
+def handle_repo(message):
+    bot.send_message(
+        message.chat.id,
+        f"{' '.join(REPO_PLAYERS)} в репу"
+    )
+
+@bot.message_handler(commands=["all"])
+def get_admins(message):
+    admin_list = get_admins_list(message.chat.id)
+    bot.send_message(message.chat.id, f"{' '.join(admin_list)}")
+
 @bot.message_handler(content_types=['document', 'audio'])
 def handle_docs_audio(message):
-    # Handle document and audio messages here
     pass
 
-# Start polling
 bot.polling(none_stop=True, interval=0)
